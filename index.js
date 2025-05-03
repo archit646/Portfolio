b158fc669944e3c0bc9f7dcd054d4c693ee810c1
Archit Kumar <architk471@gmail.com>
2025-05-03 23:40:00 +0530

    Added Js File

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.auto-typed').forEach(function(e) {
+    new Typed(e, {
+        strings: ["Web Developer", "Software Developer", "Coder"],
+        typeSpeed: 120,
+        backSpeed: 120,
+        loop: true
+    })
+})
+
