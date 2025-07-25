8c4dc8811acecb8adf4d2af1251284fc01ad60b5
Archit Kumar <architk471@gmail.com>
2025-07-25 07:10:00 +0530

    text edited

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.auto-typed').forEach(function(e) {
     new Typed(e, {
-        strings: ["Web Developer", "Software Developer", "Coder"],
+        strings: ["Web Developer","Coder","Python Developer","Full Stack Developer"],
         typeSpeed: 120,
         backSpeed: 120,
         loop: true
